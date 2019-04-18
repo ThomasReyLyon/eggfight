18c53331a7f90e420b77870f48f52a96ce8343f4
chikungugna <thomas.rey@posteo.net>
2019-04-18 14:40:26 +0200

    maj avec version plusieurs

--- src/Controller/EggsController.php
+++ src/Controller/EggsController.php
@@ -32,12 +32,12 @@ http://easteregg.wildcodeschool.fr/api-docs/
         $data=$response->getBody();
 
 
-        $data=\GuzzleHttp\json_decode($data); // Ici, ça va ressortir le resultat de la réquete de la ligne plus haut $client->request. Si random ou si :id, ça ressortira UN SEUL oeuf
+        $data=json_decode($data); // Ici, ça va ressortir le resultat de la réquete de la ligne plus haut $client->request. Si random ou si :id, ça ressortira UN SEUL oeuf
 											// DONC UN SEUL TABLEAU. SI la formule est simplement eggs/ ou characters/, ça va ressortir TOUS les oeufs. Donc UN TABLEAU DANS UN TABLEAU.
 											// Et là, c'est plus galère du coup.
 
 
-//var_dump($data);
+		//$data=array_slice(match.random_int(0,99), );
 
 
 
@@ -46,4 +46,43 @@ http://easteregg.wildcodeschool.fr/api-docs/
 
         ]);
     }
+
+	public function eggsPlusieurs()
+	{
+
+
+		$client = new Client([
+			// Base URI is used with relative requests
+			'base_uri' => 'http://easteregg.wildcodeschool.fr/api/',
+			// You can set any number of default request options.
+			'timeout'  => 2.0,
+		]);
+
+		$response = $client->request('GET', 'eggs/'); // DANS URI --> METTRE REQUETE GET que vous voulez utiliser. Liste des requêtes ci dessous.
+		/*
+				GET /api/eggs
+			GET /api/eggs/:id
+			GET /api/eggs/random
+			GET /api/characters
+			GET /api/characters/:id
+			GET /api/characters/random
+		http://easteregg.wildcodeschool.fr/api-docs/
+		*/
+		$data=$response->getBody();
+
+
+		$data=json_decode($data); // Ici, ça va ressortir le resultat de la réquete de la ligne plus haut $client->request. Si random ou si :id, ça ressortira UN SEUL oeuf
+		// DONC UN SEUL TABLEAU. SI la formule est simplement eggs/ ou characters/, ça va ressortir TOUS les oeufs. Donc UN TABLEAU DANS UN TABLEAU.
+		// Et là, c'est plus galère du coup.
+
+
+		$data=array_slice($data,random_int(0,89), 10);
+
+
+
+		return $this->twig->render('suck_my_eggs_multi.html.twig', [
+			'eggs'=>$data,
+
+		]);
+	}
 }
